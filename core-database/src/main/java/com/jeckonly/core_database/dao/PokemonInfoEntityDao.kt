5dbee2886c0ddce523daa79ad0654e6681d8524f
JeckOnly <2322802221@qq.com>
2022-10-12 23:20:16 +0800

    dao增加id和name的查找

--- core-database/src/main/java/com/jeckonly/core_database/dao/PokemonInfoEntityDao.kt
+++ core-database/src/main/java/com/jeckonly/core_database/dao/PokemonInfoEntityDao.kt
@@ -18,5 +18,15 @@ interface PokemonInfoEntityDao {
     @Query("SELECT * FROM PokemonInfoEntity WHERE page <= :page_")
     suspend fun getAllPokemonListLessThanPage(page_: Int): List<PokemonInfoEntity>
 
-    // TODO 添加对id和name的搜索
+    /**
+     * 根据id精确匹配
+     */
+    @Query("SELECT * FROM PokemonInfoEntity WHERE id == :id_")
+    suspend fun getPokemonById(id_: Int): List<PokemonInfoEntity>
+
+    /**
+     * 根据字符串模糊匹配名字
+     */
+    @Query("SELECT * FROM PokemonInfoEntity WHERE name LIKE '%' || :search || '%'")
+    suspend fun getPokemonById(search: String): List<PokemonInfoEntity>
 }
