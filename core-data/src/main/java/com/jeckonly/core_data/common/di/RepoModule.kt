75587189dedb7b98c7f5d1c6bbb8d12b17e20114
JeckOnly <2322802221@qq.com>
2022-10-17 17:05:52 +0800

    hilt配置

--- core-data/src/main/java/com/jeckonly/core_data/common/di/RepoModule.kt
+++ core-data/src/main/java/com/jeckonly/core_data/common/di/RepoModule.kt
@@ -1,7 +1,9 @@
 package com.jeckonly.core_data.common.di
 
 import com.jeckonly.core_data.common.repo.impl.PokemonRepoImpl
+import com.jeckonly.core_data.common.repo.impl.UserPrefsRepoImpl
 import com.jeckonly.core_data.common.repo.interface_.PokemonRepo
+import com.jeckonly.core_data.common.repo.interface_.UserPrefsRepo
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,8 +17,14 @@ abstract class RepoModule {
 
     @Binds
     @Singleton
-    abstract fun bindStockRepository(
+    abstract fun bindPokemonRepository(
         pokemonRepoImpl: PokemonRepoImpl
     ): PokemonRepo
+
+    @Binds
+    @Singleton
+    abstract fun bindUserPrefsRepository(
+        userPrefsRepoImpl: UserPrefsRepoImpl
+    ): UserPrefsRepo
 
 }
