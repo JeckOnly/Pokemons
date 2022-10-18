23abae23232a6b169f63601e2edf27f0b65d207b
JeckOnly <2322802221@qq.com>
2022-10-18 09:56:37 +0800

    fix bug，dto模型可为空

--- core-model/src/main/java/com/jeckonly/core_model/dto/pokemonlistitem/PokemonPageDto.kt
+++ core-model/src/main/java/com/jeckonly/core_model/dto/pokemonlistitem/PokemonPageDto.kt
@@ -2,7 +2,7 @@ package com.jeckonly.core_model.dto.pokemonlistitem
 
 data class PokemonPageDto(
     val count: Int,
-    val next: String,
+    val next: String?,
     val previous: String,
     val results: List<PokemonInfoDto>
 )
